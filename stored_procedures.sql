493639ff89ffc106bc8e925fcae3736dbf887759
CAMILOITT <camilocamilo5@outlook.com>
2022-10-12 12:28:07 -0500

    correcting errors

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,8 +1,8 @@
 CREATE TABLE teddy(
-  id serial PRIMARY key,
-  name varchar(20) NOT NULL,
-  type varchar(29) NOT NULL,
-  price NUMERIC(10,2) NOT NULL
+	id serial PRIMARY key,
+	name varchar(20) NOT NULL,
+	type varchar(29) NOT NULL,
+	price NUMERIC(10,2) NOT NULL 
 );
 
 INSERT INTO teddy(name, TYPE, price) VALUES ('oso', 'peluche', 27.99),('stich', 'peluche', 35),
@@ -10,22 +10,20 @@ INSERT INTO teddy(name, TYPE, price) VALUES ('oso', 'peluche', 27.99),('stich', 
   ('rick', 'cojin', 17),
   ('patricio Estrella', 'peluche', 6.80);
 
-SELECT * FROM teddy 
+SELECT * FROM teddy
 
 CREATE OR REPLACE PROCEDURE discount(
-  teddy_id int
+	teddy_id int
 )
 LANGUAGE plpgsql
 AS $$
 BEGIN
   UPDATE teddy
   SET price = price - (price * 0.1)
-  WHERE id = item_id;
+  WHERE id = teddy_id;
   COMMIT;
 END;$$
 
-CALL discount (1)
+CALL discount(1)
 
 SELECT * FROM teddy
-
-
